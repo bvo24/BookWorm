2619c959d3514c7fcd0a1b89c9834784ca853761
bvo24 <brianvo2020@gmail.com>
2024-05-12 17:10:21 -0400

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Brian Vo on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
